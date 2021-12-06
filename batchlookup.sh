8eafb2fac49602db085befb072142f017f92bc4f
Austin Leath <austinleath18@gmail.com>
2021-12-06 16:22:25 -0600

    short fixes, simplify script

--- batchlookup.sh
+++ batchlookup.sh
@@ -9,9 +9,9 @@ domain_list='domains.txt' # One FQDN per line in file.
 ns_ip='1.1.1.1' # Is using Cloudflare's 1.1.1.1.
 #
 # Seconds to wait between lookups:
-loop_wait='1' # Is set to 1 second.
+loop_wait='0' # Is set to 1 second.
 
-echo "Domain name, IP Address, IP PTR, IP NetName (WHOIS)"; # Start CSV
+echo "Domain name, IP Address"; # Start CSV
 for domain in `cat $domain_list` # Start looping through domains
 do
     ip=`dig @$ns_ip +short $domain |tail -n1`; # IP address lookup
@@ -19,7 +19,7 @@ do
         then # Then
             echo "$domain,No DNS"; # Write "No DNS" in the IP column
         else # And if an IP is found perform a PTR and NetName lookup
-            echo -en "$domain,$ip,"`dig @$ns_ip +short -x $ip |xargs`"\n";
+            echo "$domain,$ip";
             whois $ip |grep -i 'netname' |awk '{print $NF}'|xargs;
     fi
     sleep $loop_wait # Pause before the next lookup to avoid flooding NS
